7efbcb0b8f9545ee45b4f248eb65f29d112348ec
Shawn <da_capo_usa@hotmail.com>
2017-08-05 05:38:34 +0800

    test

--- plot.py
+++ plot.py
@@ -13,7 +13,7 @@ with open('data.csv', 'r') as csvfile:
         pointset.append(point)
         key.append(row[3])
 
-X = np.array(pointset)
+X = np.array(pointset).astype(np.int)
 y = np.array(key)
 
 # data = (pointset,key)
