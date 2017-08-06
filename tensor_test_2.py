c046e61311e9168c9ec0221aebb9beda12c09da2
Shawn <da_capo_usa@hotmail.com>
2017-08-06 13:36:43 +0800

    test

--- tensor_test_2.py
+++ tensor_test_2.py
@@ -58,6 +58,7 @@ def train_neural_network(x):
 
         for epoch in range(hm_epochs):
             epoch_loss = 0
+            print(mnist.train.num_examples)
             for _ in range(int(mnist.train.num_examples/batch_size)):
                 epoch_x, epoch_y = mnist.train.next_batch(batch_size)
                 _, c = sess.run([optimizer, cost], feed_dict = {x: epoch_x, y: epoch_y})
