2b53546459736493020a68ca01d5c1766235b43c
Shawn <da_capo_usa@hotmail.com>
2017-08-06 15:54:43 +0800

    test

--- real_tensorflow.py
+++ real_tensorflow.py
@@ -5,9 +5,9 @@ import numpy as np
 
 # mnist = input_data.read_data_sets('/tmp/data',one_hot=True)
 
-n_nodes_hl1 = 500
-n_nodes_hl2 = 500
-n_nodes_hl3 = 500
+n_nodes_hl1 = 1000
+n_nodes_hl2 = 1000
+n_nodes_hl3 = 1000
 
 n_classes = 2
 batch_size = 100
